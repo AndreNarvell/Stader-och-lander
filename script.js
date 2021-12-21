9b6920143ab5e5021a8617407764dfd14c0743a5
johanna-almqvist <94167705+johanna-almqvist@users.noreply.github.com>
2021-12-21 10:49:43 +0100

    pseudokodat

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Fetch 
+
 let citiesAndCountries = Promise.all([
   fetch("JSON/land.json").then((response) => response.json()),
   fetch("JSON/stad.json").then((response) => response.json()),
@@ -6,3 +8,29 @@ let citiesAndCountries = Promise.all([
 });
 
 console.log("C&C", citiesAndCountries);
+
+// Hämta element från HTML
+
+// Skapa menyn i header 
+    // ul med li för Sverige
+    // ul med li för Norge 
+    // ul med li för Finland
+
+// Vid klick på stad visa population i inforuta     
+
+// Loopar 
+
+// Skapa knapp
+// Spara id för populationsantal i local storage 
+
+// Skapa tömma knapp som kommer fram efter man klickat på Besökta Städer
+
+// function tömma local storage 
+
+
+ 
+
+
+
+// Besökta städer knapp
+// visa total för populationsantal 
