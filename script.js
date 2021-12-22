d039d1be8c45a7ee4ece33df63242620784b85b5
AndreNarvell <andrenarvell@hotmail.com>
2021-12-22 09:23:54 +0100

    Fixed click event, ok to push to main

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
-// Fetch 
+// Fetch
 
 let citiesAndCountries = Promise.all([
   fetch("JSON/land.json").then((response) => response.json()),
   fetch("JSON/stad.json").then((response) => response.json()),
 ]).then((data) => {
-  teams(data[0], data[1])
+  teams(data[0], data[1]);
   // console.log(data[0],data[1]);
 });
 
 // Hämta element från HTML
-let header = document.querySelector("header")
-let main = document.querySelector("main")
-let footer = document.querySelector("footer")
+let header = document.querySelector("header");
+let main = document.querySelector("main");
+let footer = document.querySelector("footer");
 
 let creatUl;
 let li;
 
 function teams(countries, cities) {
-
   for (let i = 0; i < countries.length; i++) {
+    header.insertAdjacentHTML(
+      "afterbegin",
+      '<h1 id="' + countries[i].id + '">' + countries[i].countryname + "</h1>"
+    );
 
-    let creatHeading = document.createElement("h1");
-    creatHeading.innerText = countries[i].countryname;
-    header.append(creatHeading);
+    document.getElementById(countries[i].id).addEventListener("click", () => {
+      if (document.getElementById("citiesUl")) {
+        document.getElementById("citiesUl").remove();
+      }
 
-    creatHeading.addEventListener("click", () => {
-      creatUl = document.createElement("ul");
-      
-      for (let j = 0; j < cities.length; j++) { 
+      let citiesUl = document.createElement("ul");
+      citiesUl.id = "citiesUl";
+      header.appendChild(citiesUl);
+
+      for (let j = 0; j < cities.length; j++) {
         if (countries[i].id === cities[j].countryid) {
           li = document.createElement("li");
-          
           li.innerText = cities[j].stadname;
-          // li.className = "viewCities";
-          creatUl.appendChild(li);
+          document.getElementById("citiesUl").appendChild(li);
         }
-        header.appendChild(creatUl);
       }
-      // creatUl.classList.toggle("viewCities");
-    // }, { once: true });
-  })
+    });
   }
 }
-
-
-
 
 // function removeList(list) {
 //   while (list.hasChildNodes()) {
@@ -57,26 +54,21 @@ function teams(countries, cities) {
 //   menuBtn.classList.toggle('open');
 // });
 
-// Skapa menyn i header 
-    // ul med li för Sverige
-    // ul med li för Norge 
-    // ul med li för Finland
+// Skapa menyn i header
+// ul med li för Sverige
+// ul med li för Norge
+// ul med li för Finland
 
-// Vid klick på stad visa population i inforuta     
+// Vid klick på stad visa population i inforuta
 
-// Loopar 
+// Loopar
 
 // Skapa knapp
-// Spara id för populationsantal i local storage 
+// Spara id för populationsantal i local storage
 
 // Skapa tömma knapp som kommer fram efter man klickat på Besökta Städer
 
-// function tömma local storage 
-
-
- 
-
-
+// function tömma local storage
 
 // Besökta städer knapp
 // visa total för populationsantal
