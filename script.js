29c23234b1d7828c81479f052e097716c68277bb
AndreNarvell <andrenarvell@hotmail.com>
2022-01-03 10:32:50 +0100

    Added wiki and weather fetch and some styling

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let citiesAndCountries = Promise.all([
   fetch("JSON/stad.json").then((response) => response.json()),
 ]).then((data) => {
   getData(data[0], data[1]);
-  visitCity (data[1]);
+  visitCity(data[1]);
   // console.log(data[0],data[1]);
 });
 
@@ -16,20 +16,19 @@ let footer = document.querySelector("footer");
 let visitedCities = document.getElementById("visitedCities");
 let cityId = [];
 
-function saveLocalStorage (city){
-  document.getElementById("saveBtn").addEventListener("click", () =>{
-    cityId.push(city.id)
+function saveLocalStorage(city) {
+  document.getElementById("saveBtn").addEventListener("click", () => {
+    cityId.push(city.id);
     //console.log(totPop);
-    localStorage.setItem("cityId", JSON.stringify(cityId))
-  })
-
+    localStorage.setItem("cityId", JSON.stringify(cityId));
+  });
 }
 
 function getData(countries, cities) {
   for (let i = 0; i < countries.length; i++) {
     header.insertAdjacentHTML(
       "beforeend",
-      '<h1 id="' + countries[i].id + '">' + countries[i].countryname + "</h1>"
+      "<h1 id='" + countries[i].id + "'>" + countries[i].countryname + "</h1>"
     );
 
     document.getElementById(countries[i].id).addEventListener("click", () => {
@@ -43,15 +42,16 @@ function getData(countries, cities) {
 
       for (let j = 0; j < cities.length; j++) {
         if (countries[i].id === cities[j].countryid) {
-          let city =cities[j]
+          let city = cities[j];
           let li = document.createElement("li");
           li.innerText = city.stadname;
           li.id = city.id;
           document.getElementById("citiesUl").appendChild(li);
-          
-          showInfo(li,city);
+
+          showInfo(li, city);
         }
       }
+      document.title = countries[i].countryname;
     });
   }
 }
@@ -59,88 +59,118 @@ function getData(countries, cities) {
 function showInfo(li, city) {
   // Vid klick på stad visa population i inforuta
   li.addEventListener("click", () => {
-    main.innerHTML = ""
+    main.innerHTML = "";
 
-    if (document.getElementById("infoBox") && document.getElementById("saveBtn")) {
+    if (
+      document.getElementById("infoBox") &&
+      document.getElementById("saveBtn")
+    ) {
       document.getElementById("infoBox").remove();
       document.getElementById("saveBtn").remove();
     }
- 
+
     console.log("Population" + " " + city.population);
 
-    let infoBox = document.createElement("section")
-    infoBox.id = "infoBox"
+    let infoBox = document.createElement("section");
+    infoBox.id = "infoBox";
     // infoBox.innerText = city.population
-    infoBox.innerText = `${city.stadname} har befolkningen: ${city.population}`
+    infoBox.innerText = `${city.stadname} har befolkningen: ${city.population}`;
 
     let saveBtn = document.createElement("button");
-    saveBtn.innerText = `Besökt`
-    saveBtn.id = "saveBtn"
+    saveBtn.innerText = `Besökt`;
+    saveBtn.id = "saveBtn";
     main.append(infoBox, saveBtn);
-    saveLocalStorage(city)
+    saveLocalStorage(city);
+    wwFetch(city);
   });
 }
 
-function getLocalStorage(city){
+function getLocalStorage(city) {
   cityId = JSON.parse(localStorage.getItem("cityId")) || [];
   let totPop = 0;
-  for (let i = 0; i < cityId.length; i++){
-    totPop += city[cityId[i]-1].population;
+  for (let i = 0; i < cityId.length; i++) {
+    totPop += city[cityId[i] - 1].population;
     let cityNamePrint = document.createElement("p");
-    cityNamePrint.innerText = city[cityId[i]-1].stadname;
-    cityNamePrint.id = "cityNamePrint"
+    cityNamePrint.innerText = city[cityId[i] - 1].stadname;
+    cityNamePrint.id = "cityNamePrint";
 
     document.getElementById("extraInfo").append(cityNamePrint);
   }
   let totPopPrint = document.createElement("p");
-  totPopPrint.id = "totPopPrint"
-  if (totPop != 0){
-    totPopPrint.innerText = "Den totala populationen av dina besökta städer är: " + totPop;
-  }
-  else {
+  totPopPrint.id = "totPopPrint";
+  if (totPop != 0) {
+    totPopPrint.innerText =
+      "Den totala populationen av dina besökta städer är: " + totPop;
+  } else {
     totPopPrint.innerText = "Du har inga sparade besökta städer";
-    
   }
-  document.getElementById("extraInfo").appendChild(totPopPrint)
-  
-
- 
+  document.getElementById("extraInfo").appendChild(totPopPrint);
 }
 
-function emptyLocalStorage(){
-  document.getElementById("emptyBtn").addEventListener("click", () =>{
+function emptyLocalStorage() {
+  document.getElementById("emptyBtn").addEventListener("click", () => {
     cityId = [];
     localStorage.setItem("cityId", JSON.stringify(cityId));
     document.getElementById("extraInfo").innerText = "";
     let print = document.createElement("p");
     print.innerText = "Du har inga sparade besökta städer";
     document.getElementById("extraInfo").appendChild(print);
-    
-  })
+  });
 }
 
-function visitCity (city){
+function visitCity(city) {
   visitedCities.addEventListener("click", () => {
-    main.innerHTML = ""
-    let extraInfo = document.createElement("section")
-    extraInfo.id = "extraInfo"
-    
-    let emptyBtn = document.createElement("button");
-    emptyBtn.innerText = `Tömma local Storage`
-    emptyBtn.id = "emptyBtn"
-    main.append(extraInfo)
+    main.innerHTML = "";
+    let extraInfo = document.createElement("section");
+    extraInfo.id = "extraInfo";
 
-    getLocalStorage(city)
-  
-    extraInfo.appendChild(emptyBtn)
-    emptyLocalStorage()
-    
-  })
+    let emptyBtn = document.createElement("button");
+    emptyBtn.innerText = "Rensa";
+    emptyBtn.id = "emptyBtn";
+    main.append(extraInfo);
+
+    getLocalStorage(city);
+
+    extraInfo.appendChild(emptyBtn);
+    emptyLocalStorage();
+
+    document.title = "Besökta städer";
+  });
 }
 
+function wwFetch(city) {
+  Promise.all([
+    fetch(
+      "https://sv.wikipedia.org/w/rest.php/v1/search/page?q=" +
+        city.stadname +
+        "&limit=1"
+    ).then((response) => response.json()),
 
+    fetch(
+      "https://api.weatherapi.com/v1/current.json?key=33a562cc2c244e8e857163551220201&q=" +
+        city.stadname +
+        "&aqi=no"
+    ).then((response) => response.json()),
+  ]).then((data) => {
+    document.getElementById("infoBox");
+    infoBox.innerHTML += `<p>Om staden:  ${data[0].pages[0].description}</p>`;
+    infoBox.innerHTML += `<p>Väder: ${data[1].current.temp_c}°C ${data[1].current.temp_f}°F </p>`;
+    infoBox.innerHTML += `<img src=${data[1].current.condition.icon}> </img>`;
+    infoBox.innerHTML += `<img src= ${data[0].pages[0].thumbnail.url}></img>`;
 
+    let link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+      link = document.createElement("link");
+      link.rel = "icon";
+      document.getElementsByTagName("head")[0].appendChild(link);
+    }
+    link.href = data[1].current.condition.icon;
 
+    document.title = city.stadname;
+  });
+}
+
+// ${data[1].current.condition.text}
 
 // function removeList(list) {
 //   while (list.hasChildNodes()) {
@@ -157,8 +187,6 @@ function visitCity (city){
 // ul med li för Sverige
 // ul med li för Norge
 // ul med li för Finland
-
-
 
 // Loopar
 
